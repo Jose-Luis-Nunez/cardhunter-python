a2f72dda6995df606ea8d8469998288f2dfbda65
josenunez <josenunez@outlook.de>
2024-05-07 20:20:21 +0200

    update test

--- test/CardMarketSearch.py
+++ test/CardMarketSearch.py
@@ -8,11 +8,12 @@ def search_card():
     options.add_argument('--headless')
     options.add_argument("--start-maximized")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.140 Safari/537.36")
-
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     driver = uc.Chrome(options=options)
 
     stealth(driver,
-            languages=["en-US", "en"],
+            languages=["de-DE", "de"],
             vendor="Google Inc.",
             platform="Win32",
             webgl_vendor="Intel Inc.",
