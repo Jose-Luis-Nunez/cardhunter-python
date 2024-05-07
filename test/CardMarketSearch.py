d670d343eed46c3ee75c82ad985c3c16c30ae654
josenunez <josenunez@outlook.de>
2024-05-08 00:47:23 +0200

    update test

--- test/CardMarketSearch.py
+++ test/CardMarketSearch.py
@@ -1,9 +1,22 @@
 import undetected_chromedriver as uc
 from selenium_stealth import stealth
 import time
+import random
 
 
 def search_card():
+
+    user_agents = [
+        # Add your list of user agents here
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+    ]
+    user_agent = random.choice(user_agents)
 
     options = uc.ChromeOptions()
     options.add_argument('--headless')
@@ -13,15 +26,15 @@ def search_card():
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
+    options.add_argument(f'user-agent={user_agent}')
 
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.6167.140 Safari/537.36")
     # options.add_argument('--proxy-server=http://116.202.213.226:3128')
 
     driver = uc.Chrome(options=options)
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 
     stealth(driver,
-            languages=["de-DE", "de"],
+            languages=["en-US", "en"],
             vendor="Google Inc.",
             platform="Win32",
             webgl_vendor="Intel Inc.",
