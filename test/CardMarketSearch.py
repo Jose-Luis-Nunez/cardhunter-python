c4e82103a823969ac881006875b51fd6aa5cc117
josenunez <josenunez@outlook.de>
2024-05-07 18:55:04 +0200

    update test

--- test/CardMarketSearch.py
+++ test/CardMarketSearch.py
@@ -1,10 +1,11 @@
 import undetected_chromedriver as uc
+from selenium import webdriver
 
 import time
 
 
 def search_card():
-    driver = uc.Chrome(headless=True,use_subprocess=False)
+    # driver = uc.Chrome(headless=True,use_subprocess=False)
 
     # options = uc.ChromeOptions()
     # options.headless = True
@@ -13,7 +14,13 @@ def search_card():
     # options.add_argument('--disable-gpu')
     #
     # driver = uc.Chrome(options=options)
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
 
+    driver = uc.Chrome(options=options)
     driver.get('https://www.cardmarket.com/de/Pokemon')
 
     time.sleep(10)
