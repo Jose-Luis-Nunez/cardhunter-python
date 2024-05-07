09bd5e77ef09fb257c5be55558b05964197ee802
josenunez <josenunez@outlook.de>
2024-05-07 15:55:06 +0200

    update text to use headless

--- test/CardMarketSearch.py
+++ test/CardMarketSearch.py
@@ -5,6 +5,10 @@ import time
 
 def search_card():
     options = uc.ChromeOptions()
+    options.headless = True
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
 
     driver = uc.Chrome(options=options)
 
