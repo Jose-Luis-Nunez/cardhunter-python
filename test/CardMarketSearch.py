b42e7862b13dccd8cab4f2a798bd915b9c39d602
josenunez <josenunez@outlook.de>
2024-05-07 16:09:25 +0200

    update test

--- test/CardMarketSearch.py
+++ test/CardMarketSearch.py
@@ -4,13 +4,15 @@ import time
 
 
 def search_card():
-    options = uc.ChromeOptions()
-    options.headless = True
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
+    driver = uc.Chrome(headless=True,use_subprocess=False)
 
-    driver = uc.Chrome(options=options)
+    # options = uc.ChromeOptions()
+    # options.headless = True
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-gpu')
+    #
+    # driver = uc.Chrome(options=options)
 
     driver.get('https://www.cardmarket.com/de/Pokemon')
 
